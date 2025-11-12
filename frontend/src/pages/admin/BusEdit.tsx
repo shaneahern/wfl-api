aa5913b3aada037c1c07b2ee9b3a5d0501a1b5fc
Shane Ahern <sahern@pineparkhealth.com>
2025-11-11 16:42:10 -0800

    Change Edit Bus tab label from 'Use Current Location' to 'Use Map'

--- frontend/src/pages/admin/BusEdit.tsx
+++ frontend/src/pages/admin/BusEdit.tsx
@@ -365,7 +365,7 @@ export function BusEdit() {
                         : 'text-gray-500 hover:text-gray-700'
                     }`}
                   >
-                    Use Current Location
+                    Use Map
                   </button>
                   <button
                     onClick={() => {
